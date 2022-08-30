a71b0c5aabd06045838070f5f71b9c789d82624f
Diogo Nogal Santos <diogo.nogal.santos@gmail.comm>
2022-08-30 19:06:54 +0200

    Remove dist folder

--- dist/router/song.router.d.ts
+++
@@ -1,3 +0,0 @@
-import { SongModelFactory } from '../db/song.model';
-declare const songRoute: (modelFactory: SongModelFactory) => import("express-serve-static-core").Router;
-export default songRoute;
